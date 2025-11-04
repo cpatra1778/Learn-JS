6a91604ee9ffdd07bde26cd4ec9bba5720f5e68b
Chinmay <chinmaympatra1538@gmail.com>
2025-11-04 11:35:16 +0530

    chore: basics JS (01-05)

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,22 @@
+const accountId = 15632
+let accountEmail = "cp@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "asdf@tre.com"
+accountPassword = "2587"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
